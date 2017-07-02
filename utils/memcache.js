7305692fafc9bff9558e20c1f8b5977233b9e032
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-07-02 19:03:25 +0530

    ups

--- utils/memcache.js
+++ utils/memcache.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const  Memcached = require('memcached');
-const memcached = new Memcached( process.env.MEMCACHE + ':11211',{retries:1, failures:1, retry: 300});
+
+const memcached = new Memcached( ( process.env.MEMCACHE || 'localhost' ) + ':11211',{retries:1, failures:1, retry: 300});
 
 module.exports = memcached; 
